c9e7c3dbd01e4afadcbc00fd75ecb08d7a1ecd83
hasansahara <h-m-m@outlook.com>
2024-01-11 14:47:31 +0000

    test commit

---
+++ restaurantexperts/restaurant_expert/doctype/order_types/order_types.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Hasan Al-Muhaimed and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Order Types', {
+	// refresh: function(frm) {
+
+	// }
+});
