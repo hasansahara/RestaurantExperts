9264c5cbecb3dc452ad70db7fe0a8aba91865acf
hasansahara <h-m-m@outlook.com>
2024-01-25 08:28:54 +0000

    test commit

---
+++ restaurantexperts/restaurant_expert/doctype/car_customers/test_car_customers.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Hasan Al-Muhaimed and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCarCustomers(FrappeTestCase):
+	pass
